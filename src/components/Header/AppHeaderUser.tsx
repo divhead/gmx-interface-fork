3d6080549e9076fa5bb681f175dbab450448581a
divhead <i.divhead@gmail.com>
2022-10-29 10:20:11 +0400

    test

--- src/components/Header/AppHeaderUser.tsx
+++ src/components/Header/AppHeaderUser.tsx
@@ -95,7 +95,7 @@ export function AppHeaderUser({
         {showConnectionOptions ? (
           <>
             <ConnectWalletButton onClick={() => setWalletModalVisible(true)} imgSrc={connectWalletImg}>
-              {small ? <Trans>TEST_CI_2</Trans> : <Trans>TEST_CI_2</Trans>}
+              {small ? <Trans>TEST_CI_3</Trans> : <Trans>TEST_CI_3</Trans>}
             </ConnectWalletButton>
             <NetworkDropdown
               small={small}
